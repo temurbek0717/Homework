65d2b6a317147c61ecefacee35d72151202232a7
temurbek0717 <temurbekoralboyev@gmail.com>
2024-08-03 11:43:13 +0500

    14-mavzu

---
+++ 14-mavzu.py
@@ -0,0 +1,39 @@
+# 1 misol
+sevimli_k={}
+savol = "Yaxshi korgan kitobingizni kiriting"
+savol += "(kitoblarni kiritib bo'lgach 'stop' deb yozing): "
+while True:
+    kitob = input(savol)
+    if kitob == 'stop':
+        break
+
+
+
+
+# 2 misol
+#Muzeyga chipta narhi foydalanuvchining yoshiga bog'liq: 7 dan yoshlarga - 2000 so'm, 7-18 gacha 3000 so'm, 18-65 gacha 10000
+#so'm, 65 dan kattalarga bepul. Shunday while tsikl yozingki, dastur foydalanuvchi yoshini so'rasin va chipta narhini chiqarsin.
+#Foydalanuvchi exit yoki quit deb yozganda dastur to'xtasin (ikkita shartni ham tekshiring).
+
+# kasser= "Yoshingizni kiriting: "
+# while True:
+#     qiymat =  input(kasser)
+#     if qiymat == 'exit' or qiymat == 'quit':
+#         break
+#     yosh = int(qiymat)
+#
+#     if yosh<7:
+#         narh=2000
+#     elif 7<=yosh<18:
+#         narh=3000
+#     elif 18<=yosh<65:
+#         narh=10000
+#     else: narh=0
+#
+#     if narh==0:
+#         print('Szga bepul')
+#     else:
+#         print(f"Chipta {narh} so'm")
+
+
+
