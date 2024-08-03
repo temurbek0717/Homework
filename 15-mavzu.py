2603e107b11f394dc0e25a77568f431b2aace514
temurbek0717 <temurbekoralboyev@gmail.com>
2024-08-03 11:41:51 +0500

    15-mavzu

---
+++ 15-mavzu.py
@@ -0,0 +1,10 @@
+# 3 misol
+#Foydalanuvchidan buyurtma qabul qiluvchi dastur yozing. Mahsulotlar nomini birma-bir qabul qilib, yangi ro'yxatga joylang
+# royxat=[]
+# while True:
+#     mahsulot = input("royxatga mahsulot qo'shing:")
+#     royxat.append(mahsulot)
+#     javob = input("Yana mahsulot qoshasizmi?(ha/yoq)")
+#     if javob == 'yoq':
+#         break
+# print(royxat)
