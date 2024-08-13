d5c243732c5c87e1e063bfffed5577bb56d3f235
temurbek0717 <temurbekoralboyev@gmail.com>
2024-08-13 08:42:30 +0500

    20,21-mavzu

---
+++ 20,21-mazular.py
@@ -0,0 +1,29 @@
+import math
+x=144
+print(math.sqrt(x))
+
+
+import math
+print(pow(2,5))
+
+
+
+import math
+a=30
+a_r=math.radians(a)
+sin=math.sin(a_r)
+print(sin)
+
+
+import random as r
+son=r.randint(1,100)
+print(son)
+
+
+
+import random
+colors = ["qizil", "yashil", "ko'k", "sariq", "qora"]
+color=random.choice(colors)
+print(color)
+
+
