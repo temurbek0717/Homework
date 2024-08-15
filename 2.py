8ec513083e13f0d61d5bad425b26ef42a9ccee01
temurbek0717 <temurbekoralboyev@gmail.com>
2024-08-15 08:39:59 +0500

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,5 +1,7 @@
-ism=input('Ismingiz nima?\n') # Foyalanuvchi ismini so'raymiz
-if ism !='ali': #Agar ism Aliga teng bo'lmasa...
-    print(f"Uzr,{ism.title()}biz Alini kutayapmiz.")#quyidagi xabar chiqadi
-else:
-    print("Salom,Ali")
+# ism=input('Ismingiz nima?\n') # Foyalanuvchi ismini so'raymiz
+# if ism !='ali': #Agar ism Aliga teng bo'lmasa...
+#     print(f"Uzr,{ism.title()}biz Alini kutayapmiz.")#quyidagi xabar chiqadi
+# else:
+#     print("Salom,Ali")
+
+
