850e70121125af55e3518fdbe988c037f44eb34f
temurbek0717 <temurbekoralboyev@gmail.com>
2024-09-25 22:29:00 +0500

    kunUZ

---
+++ KunUZClone/news_app/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from .models import Category,News
+# Register your models here.
+
+# 1-usul
+# admin.site.register(Category)
+# admin.site.register(News)
+
+
+# 2-usul
+@admin.register(News)
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'publish_time', 'status', 'category']
+    list_filter = ['status', 'created_time', 'publish_time']
+    prepopulated_fields = {'slug':('title',)}
+    date_hierarchy = 'publish_time'
+    search_fields = ['title']
+    ordering = ['title',]
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    search_fields = ['name']
